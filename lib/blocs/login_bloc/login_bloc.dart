e7ebcfdfba02432d8d055dadee424e74cbacce19
Evgeniy V. Radchenko <johny.live@koshmart.ru>
2024-05-03 18:45:09 +0300

    Implemented Bloc for login proccess

---
+++ lib/blocs/login_bloc/login_bloc.dart
@@ -0,0 +1,31 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:ithub_flutter_1/repos/user_repo.dart';
+
+part 'login_event.dart';
+part 'login_state.dart';
+
+class LoginBloc extends Bloc<LoginEvent, LoginState> {
+	final UserRepository _userRepository;
+	
+  LoginBloc({
+		required UserRepository userRepository
+	}) : _userRepository = userRepository,
+		super(LoginInitial()) {
+		on<LoginRequired>((event, emit) async {
+			emit(LoginProcess());
+      try {
+        await _userRepository.signIn(event.email, event.password);
+				emit(LoginSuccess());
+      } on FirebaseAuthException catch (e) {
+				emit(LoginFailure(message: e.code));
+			} catch (e) {
+				emit(const LoginFailure());
+      }
+    });
+		on<SignOutRequired>((event, emit) async {
+			await _userRepository.logOut();
+    });
+  }
+}
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,6 +1,10 @@
 import 'package:email_validator/email_validator.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:ithub_flutter_1/blocs/auth_bloc/auth_bloc.dart';
+import 'package:ithub_flutter_1/blocs/login_bloc/login_bloc.dart';
+
 import 'package:ithub_flutter_1/services/snack_bar.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -15,6 +19,8 @@ class _LoginScreenState extends State<LoginScreen> {
   TextEditingController emailInputController = TextEditingController();
   TextEditingController passwordInputController = TextEditingController();
   final formKey = GlobalKey<FormState>();
+  bool loginRequired = false;
+  
 
   @override
   void dispose() {
@@ -30,110 +36,112 @@ class _LoginScreenState extends State<LoginScreen> {
     });
   }
 
-  Future<void> login() async {
-    final navigator = Navigator.of(context);
-
-    final isValid = formKey.currentState!.validate();
-    if (!isValid) return;
-
-    try {
-      await FirebaseAuth.instance.signInWithEmailAndPassword(
-        email: emailInputController.text.trim(),
-        password: passwordInputController.text.trim(),
-      );
-    } on FirebaseAuthException catch (e) {
-      print(e.code);
-
-      if (e.code == 'invalid-credential'){
-        SnackBarService.showSnackBar(
-          context,
-          'Неправильный email или пароль. Повторите попытку',
-          true,
-        );
-        return;
-      } else {
-        SnackBarService.showSnackBar(
-          context,
-          'Неизвестная ошибка! Попробуйте еще раз.',
-          true,
-        );
-        return;
-      }
-    }
-
-    navigator.pushNamedAndRemoveUntil('/', (Route<dynamic> route) => false);
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      appBar: AppBar(
-        title: const Text('Войти'),
-        backgroundColor: Theme.of(context).primaryColor,
-        centerTitle: true,
+    return BlocProvider<LoginBloc>(
+      create: (context) => LoginBloc(
+        userRepository: context.read<AuthenticationBloc>().userRepository
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(30.0),
-        child: Form(
-          key: formKey,
-          child: Column(
-            children: [
-              TextFormField(
-                keyboardType: TextInputType.emailAddress,
-                autocorrect: false,
-                controller: emailInputController,
-                validator: (email) =>
-                    email != null && !EmailValidator.validate(email)
-                        ? 'Введите правильный Email'
-                        : null,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Введите Email',
-                ),
-              ),
-              const SizedBox(height: 30),
-              TextFormField(
-                autocorrect: false,
-                controller: passwordInputController,
-                obscureText: isHiddenPassword,
-                validator: (value) => value != null && value.length < 6
-                    ? 'Минимум 6 символов'
-                    : null,
-                autovalidateMode: AutovalidateMode.onUserInteraction,
-                decoration: InputDecoration(
-                  border: const OutlineInputBorder(),
-                  hintText: 'Введите пароль',
-                  suffix: InkWell(
-                    onTap: togglePasswordView,
-                    child: Icon(
-                      isHiddenPassword
-                          ? Icons.visibility_off
-                          : Icons.visibility,
-                      color: Colors.black,
-                    ),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 30),
-              ElevatedButton(
-                onPressed: login,
-                child: const Center(child: Text('Войти')),
-              ),
-              const SizedBox(height: 30),
-              TextButton(
-                onPressed: () => Navigator.of(context).pushNamed('/signup'),
-                child: const Text(
-                  'Регистрация',
-                  style: TextStyle(
-                    decoration: TextDecoration.underline,
-                  ),
-                ),
-              ),
-            ],
-          ),
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        appBar: AppBar(
+          title: const Text('Войти'),
+          backgroundColor: Theme.of(context).primaryColor,
+          centerTitle: true,
         ),
-      ),
+        body: Padding(
+          padding: const EdgeInsets.all(30.0),
+          child: Builder(
+            builder: (context){
+              return BlocListener<LoginBloc, LoginState>(
+                listener: (context, state){
+                  if(state is LoginSuccess) {
+                    setState(() {
+                      loginRequired = false;
+                      Navigator.pushNamedAndRemoveUntil(context, '/', (route) => false);
+                    });
+                  } else if(state is LoginProcess) {
+                    setState(() {
+                      loginRequired = true;
+                    });
+                  } else if(state is LoginFailure) {
+                    setState(() {
+                      loginRequired = false;
+                      SnackBarService.showSnackBar(
+                        context,
+                        'Неправильный email или пароль. Повторите попытку',
+                        true,
+                      );
+                    });
+                  }
+                }, 
+              child: Form(
+                key: formKey,
+                child: Column(
+                  children: [
+                    TextFormField(
+                      keyboardType: TextInputType.emailAddress,
+                      autocorrect: false,
+                      controller: emailInputController,
+                      validator: (email) =>
+                          email != null && !EmailValidator.validate(email)
+                              ? 'Введите правильный Email'
+                              : null,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Введите Email',
+                      ),
+                    ),
+                    const SizedBox(height: 30),
+                    TextFormField(
+                      autocorrect: false,
+                      controller: passwordInputController,
+                      obscureText: isHiddenPassword,
+                      validator: (value) => value != null && value.length < 6
+                          ? 'Минимум 6 символов'
+                          : null,
+                      autovalidateMode: AutovalidateMode.onUserInteraction,
+                      decoration: InputDecoration(
+                        border: const OutlineInputBorder(),
+                        hintText: 'Введите пароль',
+                        suffix: InkWell(
+                          onTap: togglePasswordView,
+                          child: Icon(
+                            isHiddenPassword
+                                ? Icons.visibility_off
+                                : Icons.visibility,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 30),
+                    ElevatedButton(
+                      onPressed: () {
+                        context.read<LoginBloc>().add(LoginRequired(
+                          emailInputController.text.trim(), 
+                          passwordInputController.text.trim()
+                        ));
+                      },
+                      child: const Center(child: Text('Войти')),
+                    ),
+                    const SizedBox(height: 30),
+                    TextButton(
+                      onPressed: () => Navigator.of(context).pushNamed('/signup'),
+                      child: const Text(
+                        'Регистрация',
+                        style: TextStyle(
+                          decoration: TextDecoration.underline,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          }),
+        )
+      )
     );
   }
 }
